3b23c59143c30a872754f3ca5f3a746e8d3b8b5f
Erdem <57351007+ErdemCmd@users.noreply.github.com>
2020-07-29 13:40:09 +0300

    Update 3dDatumTransformation.c

--- 3dDatumTransformation.c
+++ 3dDatumTransformation.c
@@ -8,8 +8,10 @@ int main()
 
  	double Xed50, Yed50, Zed50, Xdon, Ydon, Zdon, Xote, Yote, Zote, kolc;
  	double Xwgs84, Ywgs84, Zwgs84, k1, k2, k3;
+	double Xdonwgs84, Ydonwgs84, Zdonwgs84, Xotewgs84, Yotewgs84, Zotewgs84, kolcwgs84;
+	double k1wgs84, k2wgs84, k3wgs84;
 
- 			//// ED50 Datumundan WGS84 Datumuna Dönüşüm
+ 			//// ED50 Datumundan WGS84 Datumuna Dönüşüm!!!
  	/// Bilinenleri isteyelim.
  	/* ED50 Datumunda 3 Boyutlu Kartezyen Koordinatlar;
  	Xed50, Yed50, Zed50; */
@@ -59,6 +61,57 @@ int main()
 	printf("k3 değeri: %f\n", k3);
 	Zwgs84 = ((Ydon * Xed50) + (-Xdon * Yed50) + (1 * Zed50)) + k3;
 	printf("Zwgs84 değeri: %f\n", Zwgs84);
+
+			////  WGS84 Datumundan ED50  Datumuna Dönüşüm!!!
+	/// Bilinenleri isteyelim.
+	/* WGS84 Datumunda 3 Boyutlu Kartezyen Koordinatlar;
+	Xwgs84, Ywgs84, Zwgs84; */
+ 	printf("Xwgs84 değerini giriniz: ");
+	scanf("%lf", &Xwgs84);
+ 	printf("Ywgs84 değerini giriniz: ");
+	scanf("%lf", &Ywgs84);
+ 	printf("Zwgs84 değerini giriniz: ");
+	scanf("%lf", &Zwgs84);
+	/* WGS84 Datumundan ED50  Datumuna dönüşümü sağlayacak 3 Boyutlu Dönüşüm Parametreleri;
+	Xdonwgs84, Ydonwgs84, Zdonwgs84; */
+ 	printf("Xdonwgs84 değerini giriniz: ");
+	scanf("%lf", &Xdonwgs84);
+ 	printf("Ydonwgs84 değerini giriniz: ");
+	scanf("%lf", &Ydonwgs84);
+ 	printf("Zdonwgs84 değerini giriniz: ");
+	scanf("%lf", &Zdonwgs84);
+	// Xotewgs84, Yotewgs84, Zotewgs84;
+ 	printf("Xotewgs84 değerini giriniz: ");
+	scanf("%lf", &Xotewgs84);
+ 	printf("Yotewgs84 değerini giriniz: ");
+	scanf("%lf", &Yotewgs84);
+ 	printf("Zotewgs84 değerini giriniz: ");
+	scanf("%lf", &Zotewgs84);
+	// kolcwgs84;
+ 	printf("kolcwgs84 değerini giriniz: ");
+	scanf("%lf", &kolcwgs84);
+	/// Bilinmeyenlerin hesaplanması.
+	/* ED50 Datumunda 3 Boyutlu Kartezyen Koordinatlar;
+	Xed50, Yed50, Zed50;
+	k1wgs84, k2wgs84, k3wgs84;
+	k1wgs84 = (kolcwgs84 / 1000000) * Xwgs84 + Xotewgs84;
+	Xed50 = ((1 * Xwgs84) + (Zdonwgs84 * Ywgs84) + (-Ydonwgs84 * Zwgs84)) + k1wgs84;
+	k2wgs84 = (kolcwgs84 / 1000000) * Ywgs84 + Yotewgs84;
+	Yed50 = ((-Zdonwgs84 * Xwgs84) + (1 * Ywgs84) + (Xdonwgs84 * Zwgs84)) + k2wgs84;
+	k3wgs84 = (kolcwgs84 / 1000000) * Zed50 + Zotewgs84;
+	Zed50 = ((Ydonwgs84 * Xwgs84) + (-Xdonwgs84 * Ywgs84) + (1 * Zwgs84)) + k3wgs84; */
+	k1wgs84 = (kolcwgs84 / 1000000) * Xwgs84 + Xotewgs84;
+	printf("k1wgs84 değeri: %f\n", k1wgs84);
+	Xed50 = ((1 * Xwgs84) + (Zdonwgs84 * Ywgs84) + (-Ydonwgs84 * Zwgs84)) + k1wgs84;
+	printf("Xed50 değeri: %f\n", Xed50);
+	k2wgs84 = (kolcwgs84 / 1000000) * Ywgs84 + Yotewgs84;
+	printf("k2wgs84 değeri: %f\n", k2wgs84);
+	Yed50 = ((-Zdonwgs84 * Xwgs84) + (1 * Ywgs84) + (Xdonwgs84 * Zwgs84)) + k2wgs84;
+	printf("Yed50 değeri: %f\n", Yed50);
+	k3wgs84 = (kolcwgs84 / 1000000) * Zed50 + Zotewgs84;
+	printf("k3wgs84 değeri: %f\n", k3wgs84);
+	Zed50 = ((Ydonwgs84 * Xwgs84) + (-Xdonwgs84 * Ywgs84) + (1 * Zwgs84)) + k3wgs84;
+	printf("Zed50 değeri: %f\n", Zed50);
 
 	return 0;
 }
